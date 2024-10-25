9aa59ffa0199d0807ce3b3efcdc58ff539a08f85
Strocs <strocsdev@gmail.com>
2024-10-25 10:41:40 -0300

    add: vision/mision

--- src/data/SITE.ts
+++ src/data/SITE.ts
@@ -9,8 +9,10 @@ export default {
       subTitle: 'Solucionar sus problemas es nuestro trabajo',
     },
     summary: {
-      vision: '',
-      mision: '',
+      vision:
+        'En ID Construcciones, trabajamos con el compromiso de ofrecer soluciones constructivas de alta calidad que se adapten a las necesidades de cada cliente, cumpliendo con los más altos estándares de eficiencia, innovación y seguridad. Ejecutamos cada proyecto con responsabilidad y celeridad, priorizando la satisfacción del cliente y la creación de infraestructuras sostenibles. Nuestro objetivo es contribuir al desarrollo del sector a través de relaciones de confianza, trabajo en equipo y mejora continua, asegurando resultados que superen las expectativas.',
+      mision:
+        'Nos proyectamos como una empresa líder en el sector, reconocida por ofrecer soluciones eficientes, innovadoras y sostenibles. Nuestra meta es liderar mediante la mejora constante de nuestros procesos, el fortalecimiento de relaciones de confianza y la implementación de prácticas que aseguren la calidad y durabilidad de nuestras construcciones. A través de la excelencia y la mejora continua, buscamos contribuir al desarrollo del entorno construido y superar las expectativas de quienes confían en nosotros.',
     },
     projects: {
       title: 'Proyectos',
