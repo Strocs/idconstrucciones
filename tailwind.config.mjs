adcf2107a0cd415f9cf6807360ee0cad3f7ef8c6
Strocs <strocsdev@gmail.com>
2024-10-28 16:46:18 -0300

    add: new sections

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -11,5 +11,23 @@ export default {
       },
     },
   },
-  plugins: [animations],
+  plugins: [
+    animations,
+    ({ addComponents }) => {
+      addComponents({
+        '.cp-mv': {
+          clipPath: 'polygon(0 0, 100% 0, 100% 95%, 50% 100%, 50% 100%, 0 95%)',
+        },
+        '.cp-v': {
+          clipPath: 'polygon(0 0, 100% 0, 100% 85%, 50% 100%, 50% 100%, 0 85%)',
+        },
+        '.cp-rmv': {
+          clipPath: 'polygon(0 5%, 50% 0, 100% 5%, 100% 100%, 0 100%)',
+        },
+        '.cp-rv': {
+          clipPath: 'polygon(0 15%, 50% 0, 100% 15%, 100% 100%, 0 100%)',
+        },
+      })
+    },
+  ],
 }
